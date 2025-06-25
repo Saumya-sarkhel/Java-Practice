a4e541c8db9b3d094d5df5be8f1162240a3d4d3d
Saumya <saumyajitsarkhel99@gmail.com>
2025-06-25 11:42:28 +0530

    Added First class for Java Basics

---
+++ Java-Basics/src/Class-1/Add_two_number.java
@@ -0,0 +1,9 @@
+public class Add_two_number {
+  public static void main(String[] args) { // statically allocated
+		int x=20; // First variable
+		int y=10; // Seconfd variable
+		int z=x+y;
+		System.out.println("Result is:"+z);
+
+	}
+}
